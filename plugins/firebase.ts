8e2ec0f226c28afadfbbb80f4bb711501cf6820c
ekunemmanuel <emmanuelapabiekun@gmail.com>
2024-07-16 02:02:18 +0100

    del

--- plugins/firebase.ts
+++
@@ -1,9 +0,0 @@
-import { VueFire, VueFireFirestoreOptionsAPI } from "vuefire";
-
-export default defineNuxtPlugin((nuxtApp) => {
-  // const { $firebaseApp } = useNuxtApp();
-  // nuxtApp.vueApp.use(VueFire, {
-  //   firebaseApp: $firebaseApp,
-  //   modules: [VueFireFirestoreOptionsAPI()],
-  // });
-});
